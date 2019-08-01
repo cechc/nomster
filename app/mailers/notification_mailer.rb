fbd1a5aa25213f73a2c6e70149fe5cfa8e30b310
Christin Cech <christin.cech@gmail.com>
2019-08-01 01:00:42 +0000

    Email Address Change in Notification_Mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "christin.cech@gmail.com",
+    mail(to: "wiscode414@gmail.com",
       subject: "A comment has been added to your place")
   end
 end
